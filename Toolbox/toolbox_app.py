4e6318bdb65988f4c9b8c7519de425e77f3e46b5
tomke honkomp <tomke.honkomp@thuenen.de>
2025-11-11 11:52:55 +0100

    Add new entry point for multipage dashboard

--- Toolbox/toolbox_app.py
+++ Toolbox/toolbox_app.py
@@ -1,15 +1,21 @@
 import dash
-from dash import html
+from dash import html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import webbrowser
 from threading import Timer
 from pathlib import Path
-from Toolbox.classes.import_data import import_pkl_data
+from Toolbox.classes.import_data import import_pkl_data, import_formip_data
 import Toolbox.parameters.paths as toolbox_paths
 import warnings
 from Toolbox.pages.overview_db import OverviewDB
+from Toolbox.pages.forest_db import ForestDB
+from Toolbox.pages.price_db import PriceDB
+from Toolbox.pages.trade_db import TradeDB
+from Toolbox.pages.validation_db import ValidationDB
+
 
 app = dash.Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config.suppress_callback_exceptions = True
 app.title = "TiMBA Dashboards"
 PACKAGEDIR = Path(__file__).parent.parent.absolute()
 
@@ -19,9 +25,19 @@ import_pkl = import_pkl_data(num_files_to_read=5,
                              ADDINFOPATH=toolbox_paths.AIINPUTPATH)
 data = import_pkl.combined_data()
 
+import_formip_data = import_formip_data(timba_data=data,
+                                        only_baseline_sc=True,
+                                        ADDINFOPATH=toolbox_paths.AIINPUTPATH)
+formip_data = import_formip_data.load_formip_data()
+
 overview_db = OverviewDB(app=app, data=data["data_periods"], print_settings=False)
+forest_db = ForestDB(app=app, data=data["data_periods"])
+price_db = PriceDB(app=app, data=data["data_periods"])
+trade_db = TradeDB(app=app, data=data["data_periods"])
+validation_db = ValidationDB(app=app, data=formip_data)
 
 app.layout = dbc.Card([
+    dcc.Location(id="url"),
     dbc.Navbar(
         dbc.Container([
             dbc.Row([
@@ -61,15 +77,29 @@ app.layout = dbc.Card([
         ]),
         color="light",
         dark=True,
-        className="mb-3 border-3 rounded-4 shadow-sm",
+        className="mb-2 border-3 rounded-4 shadow-sm",
         style={"height": "80px"}
     ),
-
-    overview_db.app_layout
+    html.Div(id="page-content"),
 ])
 
 
-if __name__ == "__main__":
-    Timer(1, lambda: webbrowser.open_new("http://localhost:8052")).start()
+@app.callback(Output("page-content", "children"),
+              Input("url", "pathname"))
+def display_page(pathname):
+    if pathname == "/forest":
+        return forest_db.app_layout
+    elif pathname == "/price":
+        return price_db.app_layout
+    elif pathname == "/trade":
+        return trade_db.app_layout
+    elif pathname == "/validation":
+        return validation_db.app_layout
+    else:
+        return overview_db.app_layout
 
-    app.run(host='localhost', debug=False, dev_tools_ui=False, dev_tools_hot_reload=False, port=8052)
+
+if __name__ == "__main__":
+    Timer(1, lambda: webbrowser.open_new("http://localhost:8053")).start()
+
+    app.run(host='localhost', debug=False, dev_tools_ui=False, dev_tools_hot_reload=False, port=8053)
