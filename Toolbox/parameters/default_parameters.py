e164965b45c3cb3970fb02e0133240f098000f8f
tomke honkomp <tomke.honkomp@thuenen.de>
2025-12-05 11:09:19 +0100

    Add uniform color and printing style

--- Toolbox/parameters/default_parameters.py
+++ Toolbox/parameters/default_parameters.py
@@ -4,11 +4,11 @@ column_name_model = "Model"
 column_name_id = "ID"
 model_name = "TiMBA"
 
-default_plot_settings = {"line_witdh":1.5,
-                         "title_font_size":20,
-                         "font_size":16,
-                         "tick_font_size":14,
-                         "legend_font_size":14}
+default_plot_settings = {"line_witdh": 1.5,
+                         "title_font_size": 20,
+                         "font_size": 16,
+                         "tick_font_size": 14,
+                         "legend_font_size": 14}
 
 printing_plot_settings = {"line_witdh":7,
                           "title_font_size":32,
